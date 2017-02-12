1052fd83b59fab791ee69d0f8fa97e9c7e86ce96
Maryan Bahnyuk <marbuga@gmail.com>
2017-02-12 17:05:55 +0200

    add scripts and .gitignore

---
+++ scripts/use-bitbucket.bat
@@ -0,0 +1,5 @@
+git remote -v
+
+git remote set-url origin git@bitbucket.org:marbug/java.git
+
+git remote -v
---
+++ scripts/use-github.bat
@@ -0,0 +1,5 @@
+git remote -v
+
+git remote set-url origin git@github.com:marbug/java.git
+
+git remote -v
